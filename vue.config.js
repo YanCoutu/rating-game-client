4732a47583e0f8478d74b941f1e000f57f843591
Yan Coutu <yan.coutu@zerospam.ca>
2020-05-26 20:32:54 -0400

    Fix home page

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? 'rating-game-client' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/rating-game-client/' : '/',
 }
